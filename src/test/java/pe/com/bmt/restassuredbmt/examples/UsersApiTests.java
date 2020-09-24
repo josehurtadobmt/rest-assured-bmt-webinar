dc1fed536b583520640d5fedcdce2279a015bf61
Jose Hurtado Alva <jose.hurtado@bmt.pe>
2020-09-24 07:06:08 -0500

    Agregar prueba para creación de un nuevo usuario

--- src/test/java/pe/com/bmt/restassuredbmt/examples/UsersApiTests.java
+++ src/test/java/pe/com/bmt/restassuredbmt/examples/UsersApiTests.java
@@ -16,4 +16,27 @@ public class UsersApiTests extends UsersApiConfig {
         then()
                 .statusCode(200);
     }
+
+    @Test
+    public void testCreateNewUser(){
+
+        String userBody = "      {\n" +
+                "        \"name\": \"Test User\",\n" +
+                "        \"username\": \"testuser\",\n" +
+                "        \"email\": \"test@user.com\",\n" +
+                "        \"address\": {\n" +
+                "          \"street\": \"Has No Name\",\n" +
+                "          \"suite\": \"Apt. 123\",\n" +
+                "          \"city\": \"Electri\",\n" +
+                "          \"zipcode\": \"54321-6789\"\n" +
+                "        }\n" +
+                "      }";
+
+        given()
+                .body(userBody).
+                when()
+                .post("users").
+                then()
+                .statusCode(201);
+    }
 }
